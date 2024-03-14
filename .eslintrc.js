25ec05c9db5151106c114d975835df025451f023
oviniciusluz <vinicius.carraraluz@gmail.com>
2024-03-13 22:56:16 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ["universe/native"],
+  rules: {
+    "import/order": "off",
+    "prettier/prettier": ["off", { singleQuote: true }],
+  },
+};
---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Pagina inicial</Text>
+    </View>
+  );
+};
